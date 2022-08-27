e30eab592fbd9411df889a08edd08eb0abf9f710
andisiambuku <roselandambuku@gmail.com>
2022-08-27 13:46:21 +0300

    Assignment complete

--- main.js
+++ main.js
@@ -3,25 +3,6 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
-}
-
 const heartShapes = document.querySelectorAll(".like-glyph");
 const onLikeClick = function(event){
   mimicServerCall()
@@ -46,3 +27,22 @@ const onLikeClick = function(event){
 heartShapes.forEach((heartNode) =>
   heartShapes.addEventListener("click",onLikeClick)
 )
+
+//------------------------------------------------------------------------------
+// Don't change the code below: this function mocks the server response
+//------------------------------------------------------------------------------
+
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
+
+
